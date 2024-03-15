0f75da1916994fd5e0f0cb371cb8bc472cbbd1f3
spricigom <emilispricigo.ifc@gmail.com>
2024-03-15 14:02:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+  import { computed, ref } from 'vue';
+
+  const contador1 = ref(0);
+  const contador2 = ref(0);
+  const valorBooleano = ref(true);
+
+  function incrementar(contador) {
+    eval(contador).value++;
+  }
+
+  function decrementar(contador) {
+    if (eval(contador).value > 0) {
+      eval(contador).value--;
+    }
+  }
+
+  const soma = computed(() => contador1.value + contador2.value);
+  const somaPar = computed(() => soma.value % 2 === 0);
+  const somaMaiorQue10 = computed(() => soma.value > 10);
+</script>
+
+<template>
+  <div>
+    <h1>Correção dos exercícios</h1>
+    <div class="contador">
+      <h2>Contador 1</h2>
+      <button @click="incrementar('contador1')">Incrementar</button>
+      <button @click="decrementar('contador1')">Decrementar</button>
+      <p>Valor: {{ contador1 }}</p>
+    </div>
+    <div class="contador">
+      <h2>Contador 2</h2>
+      <button @click="incrementar('contador2')">Incrementar</button>
+      <button @click="decrementar('contador2')">Decrementar</button>
+      <p>Valor: {{ contador2 }}</p>
+    </div>
+    <div class="soma">
+      <h2>Soma</h2>
+      <p>Valor: {{ soma }}</p>
+      <div v-if="somaMaiorQue10">
+        <p>A soma é maior que 10</p>
+      </div>
+      <div v-else>
+        <p>A soma é menor que 10</p>
+      </div>
+      <div v-if="somaPar">
+        <p>A soma é par</p>
+      </div>
+      <div v-else>
+        <p>A soma é ímpar</p>
+      </div>
+    </div>
+    <div class="booleano">
+      <h2>Valor booleano</h2>
+      <button @click="valorBooleano = !valorBooleano">Alterar valor</button>
+      <div v-if="valorBooleano">
+        <p>Verdadeiro</p>
+      </div>
+      <div v-else>
+        <p>Falso</p>
+      </div>
+    </div>
+  </div>
+</template>
